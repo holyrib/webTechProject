dbe4d25aa6afbc0c71e47f8fee0712009daf416b
Aidana <aidana.baimbetova@gmail.com>
2019-03-19 21:29:23 +0600

    Routing added. Minor design fixes. Footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +10,14 @@ import { ItemComponent } from './item/item.component';
 import { CartComponent } from './cart/cart.component';
 import { LearnComponent } from './learn/learn.component';
 import { LookComponent } from './look/look.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ShopComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'learn', component: LearnComponent},
+  { path: 'look', component: LookComponent},
+];
 
 @NgModule({
   declarations: [
@@ -19,10 +28,12 @@ import { LookComponent } from './look/look.component';
     ItemComponent,
     CartComponent,
     LearnComponent,
-    LookComponent
+    LookComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
